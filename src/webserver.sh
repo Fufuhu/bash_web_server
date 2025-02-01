24d1f8bb4c42a07c2ff374ea9458340f45f80417
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2025-02-01 17:51:50 +0900

    レスポンスにアウトバウンドのIPアドレスを追加

--- src/webserver.sh
+++ src/webserver.sh
@@ -7,7 +7,12 @@ while true; do
     echo "ポート ${PORT} で接続待機中..."
     # netcatでポートを監視し、接続があるとHTTPレスポンスを返す
     # レスポンスヘッダーと本文を出力する
-    RESPONSE="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: $(echo -n "Hello, World!" | wc -c)\r\n\r\nHello, World!"
+
+    OUTBOUND_IP=$(curl -s https://httpbin.org/ip | jq -r .origin)
+
+    echo ${OUTBOUND_IP}
+
+    RESPONSE="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: $(echo -n "${OUTBOUND_IP}" | wc -c)\r\n\r\n${OUTBOUND_IP}"
 
     # バージョンによってはオプションが異なる場合があります
     # -l で待受、-p でポート指定、-q 1 は接続後1秒待って終了（GNU netcatなど）
